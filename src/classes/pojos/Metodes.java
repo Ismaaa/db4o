554133b0838cc0ea5961da46cdd4429cbc311dce
Ismaaa <didouhismael@gmail.com>
2017-04-14 20:02:33 +0200

    Update Metodes.java

--- src/classes/pojos/Metodes.java
+++ src/classes/pojos/Metodes.java
@@ -1,8 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* M3_Cas5_Grup 3
+*/
 package classes.pojos;
 
 import com.db4o.ObjectContainer;
@@ -13,21 +11,26 @@ import java.util.List;
 
 /**
  *
- * @author Isma
+ * @author Grup3
  */
 public class Metodes {
      /**
-     * Permite cerrar la conexion a la base de datos que se esta utilizando
-     * @param baseDeDades el ObjectContainer de la base de datos
+     * Permet tancar la connexió a la base de dades que estem utilitzant
+     * @param baseDeDades el ObjectContainer de la base de dades
      */
-    public static void cerrarConexion(ObjectContainer baseDades){
+    public static void tancarConnexio(ObjectContainer baseDades){
         try{
             baseDades.close();
         }catch(Exception e){
-            System.out.println("error al cerrar la conexion");
+            System.out.println("Error al tancar la connexió");
         }
     }
     
+    /**
+     * Desem un cicle
+     * @param baseDeDades
+     * @param cicle 
+     */
     public static void guardarCicle(ObjectContainer baseDeDades, Cicles cicle){
         try{
             baseDeDades.store(cicle);
@@ -37,6 +40,11 @@ public class Metodes {
         }
     }    
     
+    /**
+     * Desem un mòdul
+     * @param baseDeDades
+     * @param modul 
+     */
     public static void guardarModul(ObjectContainer baseDeDades, Modul modul){
         try{
             baseDeDades.store(modul);
@@ -48,9 +56,9 @@ public class Metodes {
     
     
     /**
-     * Permite almacenar un Alumne en la base de datos
-     * @param baseDeDades el objeto que representa la base de datos en la que se almacenara el alumne
-     * @param alumne el alumne que se desea almacear en la base de datos
+     * Desem un alumne
+     * @param baseDeDades
+     * @param alumne 
      */
     public static void guardarAlumne(ObjectContainer baseDeDades, Alumne alumne){
         try{
@@ -61,7 +69,11 @@ public class Metodes {
         }
     }
        
-    
+    /**
+     * Eliminem un cicle
+     * @param baseDeDades
+     * @param codi 
+     */
     public static void eliminarCicle(ObjectContainer baseDeDades,String codi){
         try{
             ObjectSet result = baseDeDades.queryByExample(new Cicles(codi,null, 0));
@@ -75,100 +87,60 @@ public class Metodes {
     
     
      /**
-     * Imprime por pantalla el resultado de una consulta sin importar el metodo
-     * de consulta
-     *
-     * @param resultado el objeto en el que se ha almacenado el resultado de la
-     * consulta
+     * Imprimeix per pantalla el resultat d'una consulta sense importar 
+     * el mètode de consulta
+     * @param resultat
      */
-    public static void imprimirResultadoConsulta(ObjectSet resultado) {
-        System.out.println("Trobats " + resultado.size() + " Objectes");
-        while (resultado.hasNext()) {
-            System.out.println(resultado.next());
+    public static void imprimirResultatConsulta(ObjectSet resultat) {
+        System.out.println("Trobats " + resultat.size() + " Objectes");
+        while (resultat.hasNext()) {
+            System.out.println(resultat.next());
         }
 
     }
     
-    public static void consultarQBEPonentesDni(ObjectContainer baseDeDades, String dni) {
-        Alumne alumne = new Alumne(dni, null, null, 0);
-        ObjectSet resultado = baseDeDades.queryByExample(alumne);
-        imprimirResultadoConsulta(resultado);
-    }    
+
     
-        /**
-     * Permite hacer una consulta mediante Query-by-Example a partir de un dni e
-     * imprimirla por pantalla
-     *
-     * @param baseDeDades la base de datos desde la que se va a operar
-     * @param dni el dni del alumne que se quiere buscar
+    /**
+     * Consultem un cicle
+     * @param baseDeDades
+     * @param codi 
      */
-    public static void consultarCicles(ObjectContainer baseDeDades, String codi) {
+    public static void consultarCicle(ObjectContainer baseDeDades, String codi) {
         Cicles cicle = new Cicles(codi, null, 0);
-        ObjectSet resultado = baseDeDades.queryByExample(cicle);
-        imprimirResultadoConsulta(resultado);
+        ObjectSet resultat = baseDeDades.queryByExample(cicle);
+        imprimirResultatConsulta(resultat);
     }
     
-    public static void consultarModuls(ObjectContainer baseDeDades, String codi) {
+    /**
+     * Consultem un els mòdul
+     * @param baseDeDades
+     * @param codi 
+     */
+    public static void consultarModul(ObjectContainer baseDeDades, String codi) {
         Modul modul = new Modul(codi, 0, null);
-        ObjectSet resultado = baseDeDades.queryByExample(modul);
-        imprimirResultadoConsulta(resultado);
+        ObjectSet resultat = baseDeDades.queryByExample(modul);
+        imprimirResultatConsulta(resultat);
     }    
     
-      /**
-     * Permite hacer una consulta mediante Query-by-Example a partir de un
-     * nombre e imprimirla por pantalla
-     *
-     * @param baseDeDades la base de datos desde la que se va a operar
-     * @param nombre el nombre del alumne que se desea encontrar
+    /**
+     * Consultem un alumne
+     * @param baseDeDades
+     * @param nom 
      */
-    public static void consultarAlumnes(ObjectContainer baseDeDades, String nom) {
+    public static void consultarAlumne(ObjectContainer baseDeDades, String nom) {
         Alumne alumne = new Alumne(null, nom, null, 0);
-        ObjectSet resultado = baseDeDades.queryByExample(alumne);
-        imprimirResultadoConsulta(resultado);
-
+        ObjectSet resultat = baseDeDades.queryByExample(alumne);
+        imprimirResultatConsulta(resultat);
     }
     
-     /**
-     * Permite hacer una consulta mediante Query-by-Example a partir de un cache
-     * imprimirla por pantalla
-     *
-     * @param baseDeDades la base de datos desde la que se va a operar
-     * @param cache el cache del alumne que se desea encontrar
-     */
-    public static void consultarQBEPonentesCache(ObjectContainer baseDeDades, String cicle) {
-        Alumne alumne = new Alumne(null, null, cicle, 0);
-        ObjectSet resultado = baseDeDades.queryByExample(alumne);
-        imprimirResultadoConsulta(resultado);
-
-    }
     
     /**
-     * Permite realizar una consulta Nativa a partir del dni e imprimirla en
-     * pantalla
-     *
-     * @param baseDeDades la base de datos desde la que se va a operar
-     * @param dni el dni del alumne que se desea encontrar
+     * Consultar comparant 
+     * @param baseDeDades
+     * @param edat 
      */
-    public static void consultarNatPonentesDni(ObjectContainer baseDeDades, final String dni) {
-        List res = baseDeDades.query(new com.db4o.query.Predicate() {
-            public boolean match(Alumne alumne) {
-                return alumne.getDNI().equalsIgnoreCase(dni);
-            }
-
-            @Override
-            public boolean match(Object et) {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
-        });
-        imprimirResultadoConsulta((ObjectSet) res);
-    }
-    /**
-     * Permite realizar una consulta Nativa y obtener a todos los ponentes que
-     * tengan un determinado cache
-     *
-     * @param baseDeDades la base de datos desde la que se va a operar
-     * @param cache el cacha que ha de tener el alumne
-     */
+    // ÉS UN EXEMPLE, HI HA QUE MODIFICAR
     public static void consulrtarNatPonentesCacheIgualA(ObjectContainer baseDeDades, final int edat) {
 
         List res = baseDeDades.query(new com.db4o.query.Predicate() {
@@ -181,59 +153,37 @@ public class Metodes {
                 throw new UnsupportedOperationException("Not supported yet.");
             }
         });
-        imprimirResultadoConsulta((ObjectSet) res);
-    }
-
-    /**
-     * Permite realizar una consulta Nativa y obtener todos los ponentes que
-     * tengan un cache superior al indicado
-     *
-     * @param baseDeDades la base de datos desde la que se va a operar
-     * @param cacheBase el cache base de los ponentes
-     */
-    public static void consultarNatPonentesCacheSuperiorA(ObjectContainer baseDeDades, final int edat) {
-
-        List res = baseDeDades.query(new com.db4o.query.Predicate() {
-            public boolean match(Alumne alumne) {
-                return alumne.getEdat() >= edat;
-            }
-
-            @Override
-            public boolean match(Object et) {
-                throw new UnsupportedOperationException("Not supported yet.");
-            }
-        });
-        imprimirResultadoConsulta((ObjectSet) res);
+        imprimirResultatConsulta((ObjectSet) res);
     }
     
     /**
-     * Permite realizar una consulta SODA cuyo resultado sean todos los ponentes
+     * Permite realizar una consulta SODA cuyo resultat sean todos los ponentes
      *
      * @param baseDeDades la base de datos desde la que se va a operar
      */
+    // ÉS UN EXEMPLE, HI HA QUE MODIFICAR
     public static void consultaSODAponentes(ObjectContainer baseDeDades) {
         Query query = baseDeDades.query();
         query.constrain(Alumne.class);//declara las restricciones
-        ObjectSet resultado = query.execute();
-        imprimirResultadoConsulta(resultado);
+        ObjectSet resultat = query.execute();
+        imprimirResultatConsulta(resultat);
     }
 
     /**
-     * Permite realizar una consulta SODA cuyo resultado sea el alumne cuyo
+     * Permite realizar una consulta SODA cuyo resultat sea el alumne cuyo
      * nombre se ha introducido por parametro
      *
      * @param baseDeDades la base de datos desde la que se va a operar
      * @param nombre el nombre del alumne que se quiere recuperar
      */
+    // ÉS UN EXEMPLE, HI HA QUE MODIFICAR
    public static void consultaSODAPonentesNombre(ObjectContainer baseDeDades, String nombre) {
         Query query = baseDeDades.query();
         query.constrain(Alumne.class);
         //creamos el constraint diciendo que el campo donde lo tiene que aplicar es nombre y la restricion es el parametro nombre
         Constraint constraint = query.descend("nombre").constrain(nombre);
-        ObjectSet resultado = query.execute();
-        imprimirResultadoConsulta(resultado);
-
-
+        ObjectSet resultat = query.execute();
+        imprimirResultatConsulta(resultat);
     } 
    
      /**
@@ -244,6 +194,7 @@ public class Metodes {
      * @param cacheInferior el cache que marca limite inferior
      * @param CacheSuperior el cache que marcha el limite superior
      */
+    // ÉS UN EXEMPLE, HI HA QUE MODIFICAR
     public static void consultaSODAPonentesCacheEntre(ObjectContainer baseDeDades, float cacheInferior, float CacheSuperior) {
         Query query = baseDeDades.query();
         query.constrain(Alumne.class);
@@ -251,8 +202,8 @@ public class Metodes {
         Constraint constraint = query.descend("cache").constrain(CacheSuperior).smaller();
         //se enlazan las dos restricciones a aplicar
         query.descend("cache").constrain(cacheInferior).greater().and(constraint);
-        ObjectSet resultado = query.execute();
-        imprimirResultadoConsulta(resultado);
+        ObjectSet resultat = query.execute();
+        imprimirResultatConsulta(resultat);
     }
 
     /**
@@ -261,13 +212,12 @@ public class Metodes {
      *
      * @param baseDeDades la base de datos desde la que se va a operar
      */
+    // ÉS UN EXEMPLE, HI HA QUE MODIFICAR
     public static void consultaSODAPonentesOrdenadosCache(ObjectContainer baseDeDades) {
         Query query = baseDeDades.query();
         query.constrain(Alumne.class);
         query.descend("cache").orderDescending();
-        ObjectSet resultado = query.execute();
-        imprimirResultadoConsulta(resultado);
-    }
-    
-    
+        ObjectSet resultat = query.execute();
+        imprimirResultatConsulta(resultat);
+    } 
 }
